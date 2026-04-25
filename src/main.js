58aeb4ed85c66908cd823a99648bad080a54ee68
Otonashi <otonashi@example.com>
2026-04-25 11:09:00 +0800

    fix: remove router/pinia, mount LandingView directly to eliminate dynamic import issues

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import router from './router'
 import './style.css'
-import App from './App.vue'
+import LandingView from './views/LandingView.vue'
 
-const app = createApp(App)
-app.use(createPinia())
-app.use(router)
+const app = createApp(LandingView)
 app.mount('#app')
